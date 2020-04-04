393223102a8126f466a8caa4fa273cb045957f6a
agatinomoschitta <agatinomoschitta@gmail.com>
2020-04-04 20:38:44 +0200

    first authentication laravel working

--- LaravelApp/app/User.php
+++ LaravelApp/app/User.php
@@ -3,8 +3,10 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
-class User extends Model
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Auth\Authenticatable as AuthenticableTrait;
+class User extends Model implements Authenticatable
 {
-    //
+    use AuthenticableTrait;
+    protected $fillable = ['name', 'email', 'password'];
 }
--- LaravelApp/routes/web.php
+++ LaravelApp/routes/web.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::resource('products', 'ProductController');
 Route::resource('orders', 'ProductController');
 Route::resource('users', 'ProductController');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
